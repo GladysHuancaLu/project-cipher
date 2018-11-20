3c1d1d204d4fcdbe2eb4b8b62724fcd31c2f07ce
GladysHuancaLu <gladys.luana123@gmail.com>
2018-11-20 13:01:44 -0500

    nuevo avance

--- app.js
+++ app.js
@@ -1,10 +1,9 @@
-const ingresar= ()=> {
-  document.getElementsByName("pantalla")[0].style.display='none';
-    document.getElementsByName("pantalla")[1].style.display='block';
-}
+
+const comenzarResponse= document.getElementById("comenzar");
 
 const cifrarResponse= document.getElementById("AquiTextoCifrado");
-const btnResponse= document.getElementById("cifrar");
+const btnCifrar= document.getElementById("cifrar");
+const btnDescifrar= document.getElementById("descifrar");
 
 const cifrar = () => {
   const offset = parseInt(document.getElementById("offset").value);
@@ -12,13 +11,45 @@ const cifrar = () => {
   let textoFinal="";
   const textoMayuscula= textoInicial.toUpperCase();
   for(var i=0; i<textoMayuscula.length; i++){
+    if(textoMayuscula[i]==" "){
+      textoFinal+=" ";
+    }
+    else{
   textoFinal+= String.fromCharCode((textoMayuscula.charCodeAt(i)-65+offset)%26+65);
-  }
+}}
 return textoFinal;
 
 }
-btnResponse.addEventListener("click" , () => {
 
+const descifrar = () => {
+  const offset = parseInt(document.getElementById("offset").value);
+  const textoInicial= document.getElementById("textoACifrar").value;
+  let textoFinal="";
+  const textoMayuscula= textoInicial.toUpperCase();
+  for(var i=0; i<textoMayuscula.length; i++){
+    if(textoMayuscula[i]==" "){
+      textoFinal+=" ";
+    }
+    else{
+  textoFinal+= String.fromCharCode((textoMayuscula.charCodeAt(i)-65-offset)%26+65);
+}}
+return textoFinal;
+
+}
+
+comenzarResponse.addEventListener("click", () =>{
+      document.getElementsByName("pantalla")[1].style.display='block';
+})
+btnCifrar.addEventListener("click" , () => {
+
+  document.getElementsByName("pantalla")[2].style.display='block';
 
 cifrarResponse.innerHTML = cifrar();
+})
+
+btnDescifrar.addEventListener("click" , () => {
+
+  document.getElementsByName("pantalla")[2].style.display='block';
+
+cifrarResponse.innerHTML = descifrar();
 })
